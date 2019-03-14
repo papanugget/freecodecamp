e6c5874841e7a2f3b2af8fb64d7b98348159abbf
papanugget <evilgenius04@gmail.com>
2019-03-14 17:14:14 -0400

    used every method to check array elements

---
+++ functional_programming/arrayEvery.js
@@ -0,0 +1,9 @@
+// Use the every method inside the checkPositive function to check if every element in arr is positive. The function should return a Boolean value.
+
+function checkPositive(arr) {
+    // Add your code below this line
+    return arr.every( currentVal => currentVal > 0)
+    
+    // Add your code above this line
+  }
+  checkPositive([1, 2, 3, -4, 5]);
